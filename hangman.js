d5a32264761bce4268f3882c2a587b7267248ba6
asing200 <147524529+asing200@users.noreply.github.com>
2025-10-18 09:33:08 -0400

    Moved Images

---
+++ hangman.js
@@ -0,0 +1,5 @@
+var POSSIBLE_WORDS =["obduraate", "verismilitude", "defenestrate",
+    "obsequious", "dissonant", "today", "idempotent"]
+
+var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+var word = POSSIBLE_WORDS[randomIndex];
