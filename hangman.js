80bbd8844edab22a867952747bc703925a531bde
asing200 <147524529+asing200@users.noreply.github.com>
2025-10-20 10:34:09 -0400

    added guess letter functionality

--- hangman.js
+++ hangman.js
@@ -1,5 +1,36 @@
 var POSSIBLE_WORDS =["obduraate", "verismilitude", "defenestrate",
     "obsequious", "dissonant", "today", "idempotent"]
+    var word ="";
+    var guesses = "";
 
-var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
-var word = POSSIBLE_WORDS[randomIndex];
+function newGame(){
+    var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+    var word = POSSIBLE_WORDS[randomIndex];
+
+
+    function guessLetter(){
+        var input = document.getElementById("guess")
+        var letter = input.value;
+        guesses += letter;
+        updatePage();
+
+    }
+    function updatePage(){
+        var clueString = "";
+        for(var i = 0; i< word.length; i++){
+            var currentLetter = word.chartAt(i);
+            if(guesses.indexOf(currentLetter) >=0 ){ //You guess it
+                clueString += currentLetter + " ";
+            }
+            else
+                clueString+= "_";
+    }   
+        var clue = document.getElementById("clue");
+        clue.innerHTML = clueString;
+
+        var guessArea = document.getElementById("guesses");
+        guessArea
+    
+    
+    }
+}
