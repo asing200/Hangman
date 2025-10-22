beb18093870a09fdd3efa5717d714e277b987ffc
asing200 <147524529+asing200@users.noreply.github.com>
2025-10-21 21:32:15 -0400

    Version of Game Logic

--- hangman.js
+++ hangman.js
@@ -4,33 +4,16 @@ var POSSIBLE_WORDS =["obduraate", "verismilitude", "defenestrate",
     var guesses = "";
 
 function newGame(){
-    var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
-    var word = POSSIBLE_WORDS[randomIndex];
+     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+     word = POSSIBLE_WORDS[randomIndex];
 
-
-    function guessLetter(){
-        var input = document.getElementById("guess")
-        var letter = input.value;
-        guesses += letter;
-        updatePage();
-
-    }
-    function updatePage(){
-        var clueString = "";
-        for(var i = 0; i< word.length; i++){
-            var currentLetter = word.chartAt(i);
-            if(guesses.indexOf(currentLetter) >=0 ){ //You guess it
-                clueString += currentLetter + " ";
-            }
-            else
-                clueString+= "_";
-    }   
+     var clueString = "";
+     for(var i = 0;i<word.length;i++)
+        {
+            clueString+= "_ ";
+        }
         var clue = document.getElementById("clue");
         clue.innerHTML = clueString;
-
-        var guessArea = document.getElementById("guesses");
-        guessArea
-    
-    
-    }
 }
+   
+    
