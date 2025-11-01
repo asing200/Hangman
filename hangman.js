b9e9eeee6084f5a5d8f4887a8152cf5cb917d75b
asing200 <147524529+asing200@users.noreply.github.com>
2025-10-31 21:23:32 -0400

    Blank out Input and Stop Multiple guesses

--- hangman.js
+++ hangman.js
@@ -2,25 +2,37 @@ var POSSIBLE_WORDS =["obduraate", "verismilitude", "defenestrate",
     "obsequious", "dissonant", "today", "idempotent"]
     var MAX_GUESSES = 6; 
     var word ="";
-    var guesses = ""
+    var guesses = "";
     var guess_count = MAX_GUESSES;
+    var gameOver = false;
 
 function newGame(){
      var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
      word = POSSIBLE_WORDS[randomIndex];
     guesses = " ";
     guess_count = MAX_GUESSES;
+    gameOver = false;
     updatePage();
 }
 function guessLetter(){
-
+    if (gameOver){
+        alert("Game Over");
+        return;
+    }
     var input = document.getElementById("guess");
     var letter = input.value;
+    if(guesses.indexOf(letter)>=0)
+    {
+        return;
+    }
     if(word.indexOf(letter) < 0 ){
+       
+       //added this in class: console.log("Empty cluestring:" );
         guess_count--;
     }
     guesses+=letter;
     updatePage();
+    input.value = "";
 } 
 function updatePage(){
      var clueString = "";
@@ -42,6 +54,8 @@ function updatePage(){
     var guessArea = document.getElementById("guesses");
     guessArea.innerHTML = "Guessed Letters: " + guesses;
 
+
+    
     //update the image
     var image = document.getElementById("hangmanImage");
     image.src="images/hangman" + guess_count + ".gif";
